5ef8d447465e052c25350163307166705bd9c73e
Christine <christine.gotth@gmail.com>
2018-04-25 16:49:06 +0200

    :lipstick: Set stage height at 50% of window

--- src/components/gui/gui.jsx
+++ src/components/gui/gui.jsx
@@ -77,8 +77,10 @@ const GUIComponent = props => {
         );
     }
 
-    const calcHeight = () => window.innerHeight - layout.topBarHeight - layout.stageHeaderHeight - 8;
-    const calcWidth = () => window.innerWidth / 3 - (8 * 2);
+    const calcHeight = () =>
+        window.innerHeight / 2 - layout.topBarHeight - layout.stageHeaderHeight - 8;
+    const calcWidth = () =>
+        window.innerWidth / 3 - (8 * 2);
 
     const saveAction = () => {
         onSaveModalError("");
